670b0b6f8098e5d4f1c454966f2368abbb1f9a9d
Kyle Barton <kyle.humberto@gmail.com>
2018-12-03 10:46:59 -0800

    FIX links can be nil

--- lib/graphiti/hash_renderer.rb
+++ lib/graphiti/hash_renderer.rb
@@ -36,7 +36,7 @@ module Graphiti
       opts = options.slice(:fields, :include)
       to_hash(serializers, opts).tap do |hash|
         hash.merge!(options.slice(:meta)) if !options[:meta].empty?
-        hash.merge!(options.slice(:links)) if !options[:links].empty?
+        hash.merge!(options.slice(:links)) if options[:links]
       end
     end
 
