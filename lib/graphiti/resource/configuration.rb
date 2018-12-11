4ba9583e309b3ea292f7a80788e9861e10c845a8
Kyle Barton <kyle.humberto@gmail.com>
2018-12-11 19:19:40 +0200

    FIX remove links as per code review

--- lib/graphiti/resource/configuration.rb
+++ lib/graphiti/resource/configuration.rb
@@ -170,7 +170,6 @@ module Graphiti
               filters: {},
               default_filters: {},
               stats: {},
-              links: nil,
               sort_all: nil,
               sorts: {},
               pagination: nil,
