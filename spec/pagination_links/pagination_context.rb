783f50e0f595a3fd27a97d5bab166dcef2bb2138
Kyle Barton <kyle.humberto@gmail.com>
2018-12-19 08:28:58 +0200

    Move rspec configuration into spec_helper
    FIXES: https://github.com/graphiti-api/graphiti/pull/58#discussion_r241851991

--- spec/pagination_links/pagination_context.rb
+++ spec/pagination_links/pagination_context.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-RSpec.configure do |rspec|
-  # This config option will be enabled by default on RSpec 4,
-  # but for reasons of backwards compatibility, you have to
-  # set it on RSpec 3.
-  #
-  # It causes the host group and examples to inherit metadata
-  # from the shared context.
-  rspec.shared_context_metadata_behavior = :apply_to_host_groups
-end
-
 RSpec.shared_context "pagination_context", shared_context: :metadata do
   let(:proxy)      { double(resource: resource, query: query, scope: scope) }
   let(:resource)   { double(endpoint: endpoint) }
@@ -45,9 +35,4 @@ RSpec.shared_context "pagination_context", shared_context: :metadata do
       actions: [:index, :show, :create, :update, :destroy]
     }
   end
-  let(:kaminari_backend){ Graphiti::PaginationLinks::KaminariBackend.new(proxy) }
-end
-
-RSpec.configure do |rspec|
-  rspec.include_context "pagination_context", include_shared: true
 end
